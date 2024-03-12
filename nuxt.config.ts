03946f5511cef22085f8a673d1fdea5adf1fa803
Aleksey <svetlampy@mail.ru>
2024-03-12 14:52:38 +0500

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+module.exports = {
+  server: {
+    host: '0.0.0.0'
+  },
+}
+export default defineNuxtConfig({
+  devtools: { enabled: true }
+})
